c9f46a2ba97c5f9b45442d5fb61587d078386ad0
StevenLeandro <88866476+StevenLeandro@users.noreply.github.com>
2022-03-09 18:13:09 -0800

    Add files via upload

---
+++ scripts/card-function.js
@@ -0,0 +1,17 @@
+const card = document.querySelector('.card1__flip');
+
+card.addEventListener('click', function(){
+    card.classList.toggle('is-flipped');
+});
+
+const card2 = document.querySelector('.card2__flip');
+
+card2.addEventListener('click', function(){
+    card2.classList.toggle('is-flipped');
+});
+
+const card3 = document.querySelector('.card3__flip');
+
+card3.addEventListener('click', function(){
+    card3.classList.toggle('is-flipped');
+});
---
+++ scripts/formSucess.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", function() {
+  const form = document.getElementById('my-form');
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      alert("Thank you! We will call you with your offer within 48 hours!");
+      $(".alert").alert('close');
+
+    })
+  });
+});
